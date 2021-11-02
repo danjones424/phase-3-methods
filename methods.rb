7815b9676973b60f81b6d286dfb0551d3a1345b5
Daniel <daniel.jones424@gmail.com>
2021-11-01 23:00:00 -0400

    commit tree c5ae64cb41bedbe38451c0e9a328bd5a132c5191

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+
+def greet_programmer
+    puts 'Hello, programmer!'
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name='programmer'
+    puts "Hello, #{name}!"
+end
+
+def add num_1, num_2
+    num_1 + num_2
+end
+
+def halve num 
+    if num.to_i == num
+        num / 2
+    else 
+        nil
+    end
+end
+
